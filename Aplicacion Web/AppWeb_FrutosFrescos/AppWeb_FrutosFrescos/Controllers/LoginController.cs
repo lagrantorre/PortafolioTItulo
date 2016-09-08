1f437af66e54305e8c105c29bdcfb4b28dea1509
DESKTOP-LA0PPJ3\luisu <lagrantorre@hotmail.com>
2016-09-08 20:32:44 -0300

    Subida inicial de aplicaciones y librerias de java

---
+++ Aplicacion Web/AppWeb_FrutosFrescos/AppWeb_FrutosFrescos/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppWeb_FrutosFrescos.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index(string error = "")
+        {
+            ViewBag.Error = error;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ingreso(DataUser data)
+        {
+
+            var wea = data;
+
+            if (wea.User == "Felipe" && wea.Pass == "123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+               
+                return RedirectToAction("Index", "Login", new { error = "Usuario no existe o la contraseña es incorrecta" });
+            }
+        }
+    }
+
+
+    public class DataUser
+    {
+        public string User { get; set; }
+        public string Pass { get; set; }
+    }
+}
---
+++ Aplicacion Web/FrutosFrescosWebApp/FrutosFrescosWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FrutosFrescosWebApp.Startup))]
+namespace FrutosFrescosWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
